252de681edc7f9462c8e0447be113760f04166e1
ssongjay <jay5326@naver.com>
2022-11-11 17:16:18 +0900

    add ft_memset

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,10 +6,23 @@
 /*   By: injsong <injsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 21:43:35 by injsong           #+#    #+#             */
-/*   Updated: 2022/11/09 21:43:53 by injsong          ###   ########.fr       */
+/*   Updated: 2022/11/11 16:38:45 by injsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdlib.h>
 
-
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t i;
+	unsigned char *tmp;
+	
+	b = (unsigned char)tmp;
+	i = 0;
+	while (i < len)
+	{
+		tmp[i] = (unsigned char)c;
+		i++;
+	}
+	return (tmp);
+}
