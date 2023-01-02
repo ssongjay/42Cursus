bf0d07bf986723d71feb1d4e896ec34b11a4fe60
ssongjay <jay5326@naver.com>
2023-01-02 15:04:04 +0900

    add return condition function null

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: song-inje <song-inje@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:38:09 by song-inje         #+#    #+#             */
-/*   Updated: 2023/01/02 14:56:26 by song-inje        ###   ########.fr       */
+/*   Updated: 2023/01/02 15:03:14 by song-inje        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int i;
 
-	if (!s)
+	if (!s || !f)
 		return;
 	while (s[i] != '\0')
 	{
