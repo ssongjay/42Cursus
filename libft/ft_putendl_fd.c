4a7770b43f5dc12854d3a959204a7356ea6491cf
ssongjay <jay5326@naver.com>
2023-01-03 14:17:14 +0900

    add condition string null

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: song-inje <song-inje@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:31:31 by song-inje         #+#    #+#             */
-/*   Updated: 2023/01/02 15:48:04 by song-inje        ###   ########.fr       */
+/*   Updated: 2023/01/02 18:49:01 by song-inje        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return;
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: song-inje <song-inje@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:21:28 by song-inje         #+#    #+#             */
-/*   Updated: 2023/01/02 15:35:38 by song-inje        ###   ########.fr       */
+/*   Updated: 2023/01/02 18:49:18 by song-inje        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 void ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return;
 	write(fd, s, ft_strlen(s));
 }
