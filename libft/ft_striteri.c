0d14389964df6b359c8d4d7c939c8e436f80f80b
ssongjay <jay5326@naver.com>
2023-01-04 19:26:13 +0900

    check norminette

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -3,21 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: song-inje <song-inje@student.42.fr>        +#+  +:+       +#+        */
+/*   By: injsong <injsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:38:09 by song-inje         #+#    #+#             */
-/*   Updated: 2023/01/02 15:03:14 by song-inje        ###   ########.fr       */
+/*   Updated: 2023/01/04 18:04:58 by injsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char *))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	unsigned int i;
+	unsigned int	i;
 
 	if (!s || !f)
-		return;
+		return ;
+	i = 0;
 	while (s[i] != '\0')
 	{
 		f(i, s + i);
