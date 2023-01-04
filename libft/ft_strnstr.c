21a4de939c0fce0d418c7c1ce57f780c8b637850
ssongjay <jay5326@naver.com>
2023-01-04 19:27:26 +0900

    haystack null 파라미터 경우 종료 조건 추가

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: injsong <injsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 22:32:18 by song-inje         #+#    #+#             */
-/*   Updated: 2022/12/28 18:56:03 by injsong          ###   ########.fr       */
+/*   Updated: 2023/01/04 17:43:50 by injsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 	size_t	j;
 
+	if (!haystack && !len)
+		return (0);
 	if (*needle == '\0')
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] != '\0' && i < len)
+	while (*haystack && i < len)
 	{
 		j = 0;
 		if (haystack[i] == needle[j])
 		{
-			while ((haystack[i + j] == needle[j]) && (i + j) < len)
+			while ((haystack[i + j] == needle[j])
+				&& haystack[i + j] != '\0'
+				&& (i + j) < len)
 				j++;
 			if (needle[j] == '\0')
 				return ((char *)&haystack[i]);
