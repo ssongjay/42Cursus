07076a75972b4c9c51b6b8409ad9c83a8fbcc758
ssongjay <jay5326@naver.com>
2023-01-21 21:46:41 +0900

    add: str null 값 들어올 떄 개행문자 출력하도록 변경 , fd 음수일 때 처리 추가

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,17 +6,16 @@
 /*   By: injsong <injsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 15:31:31 by song-inje         #+#    #+#             */
-/*   Updated: 2023/01/19 14:16:54 by injsong          ###   ########.fr       */
+/*   Updated: 2023/01/21 16:30:53 by injsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	if (!s)
+	if (fd < 0)
 		return ;
-	write(fd, s, ft_strlen(s));
-	write(fd, "\n", 1);
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
