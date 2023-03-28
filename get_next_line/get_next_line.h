588b1052d78d4b14334676db904e37541280d2f9
ssongjay <jay5326@naver.com>
2023-03-28 17:45:13 +0900

    fix: BUFFER_SIZE define

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,24 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: song-inje <song-inje@student.42.fr>        +#+  +:+       +#+        */
+/*   By: injsong <injsong@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 16:22:50 by injsong           #+#    #+#             */
-/*   Updated: 2023/03/28 03:24:33 by song-inje        ###   ########.fr       */
+/*   Updated: 2023/03/28 17:39:57 by injsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char *get_next_line(int fd);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-size_t ft_strlen(char *s);
-char *ft_strchr(char *str, int ch);
-void *ft_memcpy(void *dst, const void *src, size_t n);
-char *ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *str, int ch);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
